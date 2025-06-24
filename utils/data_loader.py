3625fce0ca362cf846943a282f6e50547db9bf3c
diogormdias97 <diogormdias97@gmail.com>
2025-06-24 17:46:36 +0100

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,15 +1,11 @@
 import pandas as pd
+import streamlit as st
 
-def load_data():
-    """
-    Carrega os dados dos jogadores a partir do ficheiro CSV.
-    Retorna um DataFrame do pandas.
-    """
-    try:
-        df = pd.read_csv("data/players_data.csv")
-        return df
-    except FileNotFoundError:
-        raise FileNotFoundError("O ficheiro 'players_data.csv' não foi encontrado na pasta 'data/'.")
-    except Exception as e:
-        raise RuntimeError(f"Ocorreu um erro ao carregar os dados dos jogadores: {e}")
+@st.cache_data
+def load_players():
+    return pd.read_csv("data/players_data.csv")
+
+@st.cache_data
+def load_games():
+    return pd.read_csv("data/games.csv")
 
